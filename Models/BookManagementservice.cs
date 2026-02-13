361d184f366d84f191201d6cc3670f661013be9b
Robert Heinbokel <RobertHeinbokel@gmail.com>
2026-02-12 19:42:22 -0500

    Promted user

---
+++ Models/BookManagementservice.cs
@@ -0,0 +1,10 @@
+public class BookManagementservice
+{
+  public void OnStart ()
+  {Console.WriteLine  ("Welcome to the book management system. You currently have 2 books in your system. What would you like to do \n 1. Display books \n 2. Display Book by Book ID \n 3. Add New Book \n 4. Remove Book by Book ID \n 5. Exit");
+
+string UserResponse = Console.ReadLine();
+
+  }
+}
+
